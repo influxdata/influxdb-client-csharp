4b75075cecd551488113bd4feb9b5e97eb2fe2a5
Jakub Bednar <jakub.bednar@gmail.com>
2020-01-22 12:58:55 +0100

    chore(ci): deploy dev version into Nuget repository

--- Scripts/ci-deploy-snapshot.sh
+++ Scripts/ci-deploy-snapshot.sh
@@ -3,16 +3,15 @@
 set -e
 
 #
-# Lookup to Client Version
+# Nuget repository
 #
-dotnet tool install --tool-path="./DotnetVersion/" dotnet-version-cli 
-VERSION="$(./DotnetVersion/dotnet-version -f Client.Core/Client.Core.csproj | tail -1 | awk '{$1=$1};1')"
-echo $VERSION
+DEFAULT_BONITOO_NUGET_URL="https://apitea.com/nexus/service/local/nuget/bonitoo-nuget/"
+BONITOO_NUGET_URL="${BONITOO_NUGET_URL:-$DEFAULT_BONITOO_NUGET_URL}"
 
 #
 # Deploy to Preview repository
 #
-dotnet pack
-dotnet nuget push ./Client.Core/bin/Debug/InfluxDB.Client.Core.$VERSION.nupkg -s https://apitea.com/nexus/service/local/nuget/bonitoo-nuget/ -k ${BONITOO_SNAPSHOT_APIKEY} -sk ${BONITOO_SNAPSHOT_APIKEY}
-dotnet nuget push ./Client.Legacy/bin/Debug/InfluxDB.Client.Flux.$VERSION.nupkg -s https://apitea.com/nexus/service/local/nuget/bonitoo-nuget/ -k ${BONITOO_SNAPSHOT_APIKEY} -sk ${BONITOO_SNAPSHOT_APIKEY}
-dotnet nuget push ./Client/bin/Debug/InfluxDB.Client.$VERSION.nupkg -s https://apitea.com/nexus/service/local/nuget/bonitoo-nuget/ -k ${BONITOO_SNAPSHOT_APIKEY} -sk ${BONITOO_SNAPSHOT_APIKEY}
+dotnet pack --version-suffix=dev.$CIRCLE_BUILD_NUM
+dotnet nuget push ./Client.Core/bin/Debug/InfluxDB.Client.Core.*-dev.$CIRCLE_BUILD_NUM.nupkg -s ${BONITOO_NUGET_URL} -k ${BONITOO_SNAPSHOT_APIKEY} -sk ${BONITOO_SNAPSHOT_APIKEY}
+dotnet nuget push ./Client.Legacy/bin/Debug/InfluxDB.Client.Flux.*-dev.$CIRCLE_BUILD_NUM.nupkg -s ${BONITOO_NUGET_URL} -k ${BONITOO_SNAPSHOT_APIKEY} -sk ${BONITOO_SNAPSHOT_APIKEY}
+dotnet nuget push ./Client/bin/Debug/InfluxDB.Client.dev.*-$CIRCLE_BUILD_NUM.nupkg -s ${BONITOO_NUGET_URL} -k ${BONITOO_SNAPSHOT_APIKEY} -sk ${BONITOO_SNAPSHOT_APIKEY}
