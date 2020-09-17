5612aed4af6d0143a3605afa2f7ba29250fc4db2
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2020-09-16 23:31:29 -0500

    test: added test for poco types with timestamps

--- Client.Legacy.Test/FluxResultMapperTest.cs
+++ Client.Legacy.Test/FluxResultMapperTest.cs
@@ -1,6 +1,8 @@
+using System;
 using InfluxDB.Client.Core;
 using InfluxDB.Client.Core.Flux.Domain;
 using InfluxDB.Client.Core.Flux.Internal;
+using NodaTime;
 using NUnit.Framework;
 
 namespace Client.Legacy.Test
@@ -36,6 +38,66 @@ namespace Client.Legacy.Test
 
             [Column("region")]
             public string DifferentName { get; set; }
+        }
+
+        [Test]
+        public void MapTimestampWithInstantTime()
+        {
+            var now = DateTime.UtcNow;
+
+            var fluxRecord = new FluxRecord(0);
+            fluxRecord.Values["tag"] = "production";
+            fluxRecord.Values["min"] = 10.5;
+            fluxRecord.Values["max"] = 20.0;
+            fluxRecord.Values["avg"] = 18.0D;
+            fluxRecord.Values["_time"] = Instant.FromDateTimeUtc(now);
+
+            var poco = _parser.ToPoco<PointWithoutTimestampNameInstant>(fluxRecord);
+
+            Assert.AreEqual("production", poco.Tag);
+            Assert.AreEqual(10.5, poco.Minimum);
+            Assert.AreEqual(20, poco.Maximum);
+            Assert.AreEqual(18, poco.Average);
+            Assert.AreEqual(Instant.FromDateTimeUtc(now), poco.Timestamp);
+        }
+
+        private class PointWithoutTimestampNameInstant
+        {
+            [Column("tag", IsTag = true)] public string Tag { get; set; }
+            [Column("min")] public double Minimum { get; set; }
+            [Column("max")] public double Maximum { get; set; }
+            [Column("avg")] public double Average { get; set; }
+            [Column(IsTimestamp = true)] public Instant Timestamp { get; set; }
+        }
+
+        [Test]
+        public void MapTimestampDifferentName()
+        {
+            var now = DateTime.UtcNow;
+
+            var fluxRecord = new FluxRecord(0);
+            fluxRecord.Values["tag"] = "production";
+            fluxRecord.Values["min"] = 10.5;
+            fluxRecord.Values["max"] = 20.0;
+            fluxRecord.Values["avg"] = (Double)18;
+            fluxRecord.Values["_time"] = Instant.FromDateTimeUtc(now);
+
+            var poco = _parser.ToPoco<PointWithoutTimestampName>(fluxRecord);
+
+            Assert.AreEqual("production", poco.Tag);
+            Assert.AreEqual(10.5, poco.Minimum);
+            Assert.AreEqual(20, poco.Maximum);
+            Assert.AreEqual(18, poco.Average);
+            Assert.AreEqual(now, poco.Timestamp);
+        }
+
+        private class PointWithoutTimestampName
+        {
+            [Column("tag", IsTag = true)] public string Tag { get; set; }
+            [Column("min")] public double Minimum { get; set; }
+            [Column("max")] public double Maximum { get; set; }
+            [Column("avg")] public Double Average { get; set; }
+            [Column(IsTimestamp = true)] public DateTime Timestamp { get; set; }
         }
     }
 }
