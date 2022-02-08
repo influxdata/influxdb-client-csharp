a6849331b38d2236ffc0d4587e2aa3f2c846ddff
Jakub Bednar <jakub.bednar@gmail.com>
2022-02-08 11:31:18 +0100

    fix: test

--- Client.Test/WriteApiTest.cs
+++ Client.Test/WriteApiTest.cs
@@ -413,7 +413,7 @@ namespace InfluxDB.Client.Test
             listener.Get<WriteSuccessEvent>();
             
             var request= MockServer.LogEntries.Last();
-            StringAssert.StartsWith("influxdb-client-csharp/3.", request.RequestMessage.Headers["User-Agent"].First());
+            StringAssert.StartsWith("influxdb-client-csharp/4.", request.RequestMessage.Headers["User-Agent"].First());
             StringAssert.EndsWith(".0.0", request.RequestMessage.Headers["User-Agent"].First());
         }
 
