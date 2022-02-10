a8cb6bfa6de617d8f07c8cdf59da941b930f0d5f
Jakub Bednar <jakub.bednar@gmail.com>
2022-02-10 13:10:15 +0100

    fix: code style

--- Client.Core/Internal/RestSharpExtensions.cs
+++ Client.Core/Internal/RestSharpExtensions.cs
@@ -8,19 +8,20 @@ using RestSharp;
 
 namespace InfluxDB.Client.Core.Internal
 {
-    static class RestSharpExtensions
+    internal static class RestSharpExtensions
     {
         internal static IEnumerable<HeaderParameter> ToHeaderParameters(this HttpHeaders httpHeaders)
-            => httpHeaders
+        {
+            return httpHeaders
                 .SelectMany(x => x.Value.Select(y => (x.Key, y)))
                 .Select(x => new HeaderParameter(x.Key, x.y));
+        }
 
         internal static RestRequest AddAdvancedResponseHandler(this RestRequest restRequest,
             Func<HttpResponseMessage, RestResponse> advancedResponseWriter)
         {
-            const BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
-                                           | BindingFlags.Static | BindingFlags.DeclaredOnly;
-            var field = restRequest.GetType().GetField("_advancedResponseHandler", bindFlags);
+            var field = restRequest.GetType()
+                .GetField("_advancedResponseHandler", BindingFlags.Instance | BindingFlags.NonPublic);
             field!.SetValue(restRequest, advancedResponseWriter);
 
             return restRequest;
