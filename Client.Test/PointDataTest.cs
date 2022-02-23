3ca0fbb3cca1b8494b3201c160d9c33705f628e2
Jakub Bednar <jakub.bednar@gmail.com>
2022-02-23 09:54:09 +0100

    docs: update CHANGELOG.md

--- Client.Test/PointDataTest.cs
+++ Client.Test/PointDataTest.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 using InfluxDB.Client.Api.Domain;
 using InfluxDB.Client.Writes;
 using NodaTime.Text;
@@ -417,6 +420,29 @@ namespace InfluxDB.Client.Test
                 .Field("flout-nan", float.NaN);
 
             Assert.AreEqual("", point.ToLineProtocol());
+        }
+
+        [Test]
+        public void ToLineProtocolPerformance()
+        {
+            var points = new List<PointData>();
+            for (var i = 0; i < 1000000; i++)
+            {
+                points.Add(PointData.Measurement("h2o")
+                    .Tag("location", "europe")
+                    .Field("level", 2));
+            }
+            
+            var pointSettings = new PointSettings();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            foreach (var point in points)
+            {
+                point.ToLineProtocol(pointSettings);
+            }
+            stopwatch.Stop();
+            
+            Console.WriteLine($"Elapsed milliseconds: {stopwatch.Elapsed.TotalMilliseconds}");
         }
     }
 }
