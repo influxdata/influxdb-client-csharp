4593261fe1deb71bf37486c050c01aba80f8ac07
Jakub Bednar <jakub.bednar@gmail.com>
2022-04-05 09:18:09 +0200

    docs: add an example fix create, update, list, delete

--- Examples/InvocableScripts.cs
+++ Examples/InvocableScripts.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Threading.Tasks;
 using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Core;
+using InfluxDB.Client.Writes;
 
 namespace Examples
 {
@@ -24,6 +28,61 @@ namespace Examples
                 .Build();
 
             using var client = InfluxDBClientFactory.Create(options);
+            client.SetLogLevel(LogLevel.Body);
+
+            //
+            // Prepare data
+            //
+            var point1 = PointData.Measurement("my_measurement")
+                .Tag("location", "Prague")
+                .Field("temperature", 25.3);
+            var point2 = PointData.Measurement("my_measurement")
+                .Tag("location", "New York")
+                .Field("temperature", 24.3);
+
+            await client.GetWriteApiAsync().WritePointsAsync(new[] { point1, point2 });
+
+            var scriptsApi = client.GetInvocableScriptsApi();
+
+            //
+            // Create Invocable Script
+            //
+            Console.WriteLine("------- Create -------\n");
+            const string scriptQuery = "from(bucket: params.bucket_name) |> range(start: -30d) |> limit(n:2)";
+            var createRequest = new ScriptCreateRequest(
+                $"my_script_{DateTime.Now.Ticks}",
+                "my first try",
+                scriptQuery,
+                ScriptLanguage.Flux);
+
+            var createdScript = await scriptsApi.CreateScriptAsync(createRequest);
+            Console.WriteLine(createdScript);
+
+            //
+            // Update Invocable Script
+            //
+            Console.WriteLine("------- Update -------\n");
+            var updateRequest = new ScriptUpdateRequest(description: "my updated description");
+            createdScript = await scriptsApi.UpdateScriptAsync(createdScript.Id, updateRequest);
+            Console.WriteLine(createdScript);
+
+            //
+            // List scripts
+            //
+            Console.WriteLine("\n------- List -------\n");
+            var scripts = await scriptsApi.FindScriptsAsync();
+            foreach (var script in scripts)
+            {
+                Console.WriteLine($" ---\n ID: {script.Id}\n Name: {script.Name}\n Description: {script.Description}");
+            }
+            Console.WriteLine("---");
+
+            //
+            // Delete previously created Script
+            //
+            Console.WriteLine("------- Delete -------\n");
+            await scriptsApi.DeleteScriptAsync(createdScript.Id);
+            Console.WriteLine($"Successfully deleted script: '{createdScript.Name}'");
         }
     }
 }
