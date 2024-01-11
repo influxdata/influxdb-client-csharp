5c0092fdc3ffe53d7adfaacb1249e72f66f5d969
Sylvain Bruyère <sylvain_bruyere@live.fr>
2024-01-11 23:20:48 +0100

    chore: use C# 12, add `.NET 8.0` target framework to test projects and align .csproj file

--- Client.Linq/Internal/QueryExecutor.cs
+++ Client.Linq/Internal/QueryExecutor.cs
@@ -8,12 +8,6 @@ using InfluxDB.Client.Api.Domain;
 using InfluxDB.Client.Core.Flux.Domain;
 using Remotion.Linq;
 
-[assembly: InternalsVisibleTo("Client.Linq.Test, PublicKey=002400000480000094000000060200000024000052534131" +
-                              "0004000001000100efaac865f88dd35c90dc548945405aae34056eedbe42cad60971f89a861a78437e86d" +
-                              "95804a1aeeb0de18ac3728782f9dc8dbae2e806167a8bb64c0402278edcefd78c13dbe7f8d13de36eb362" +
-                              "21ec215c66ee2dfe7943de97b869c5eea4d92f92d345ced67de5ac8fc3cd2f8dd7e3c0c53bdb0cc433af8" +
-                              "59033d069cad397a7")]
-
 namespace InfluxDB.Client.Linq.Internal
 {
     /// <summary>
