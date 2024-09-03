c723766d54f11f8fdb2c33d967275952c8d1b3e0
alespour <42931850+alespour@users.noreply.github.com>
2024-09-03 17:37:52 +0200

    test: add test for server returning 201 Created for writes (#665)

--- Client.Test/WriteApiTest.cs
+++ Client.Test/WriteApiTest.cs
@@ -382,6 +382,27 @@ namespace InfluxDB.Client.Test
         }
 
         [Test]
+        public void Created()
+        {
+            var listener = new EventListener(_writeApi);
+
+            MockServer.Reset();
+            MockServer
+                .Given(Request.Create().UsingPost())
+                .RespondWith(CreateResponse(
+                    "OK",
+                    201));
+
+            _writeApi.WriteRecord("h2o_feet,location=coyote_creek level\\ description=\"feet 1\",water_level=1.0 1",
+                WritePrecision.Ns, "b1", "org1");
+            _writeApi.Flush();
+
+            var writeSuccessEvent = listener.Get<WriteSuccessEvent>();
+            Assert.AreEqual("h2o_feet,location=coyote_creek level\\ description=\"feet 1\",water_level=1.0 1",
+                writeSuccessEvent.LineProtocol);
+        }
+
+        [Test]
         public void TwiceDispose()
         {
             _writeApi.Dispose();
