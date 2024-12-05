d0c2a1651a6685a3d430bd0939763d6c1d49b288
Jakub Bednar <jakub.bednar@gmail.com>
2024-12-05 09:29:15 +0100

    feat: default dotnet image to 8

--- Scripts/ci-test.sh
+++ Scripts/ci-test.sh
@@ -42,9 +42,9 @@ else
   TRX2JUNIT_VERSION="1.3.2"
 fi
 
-if [[ "$NET_TEST_VERSION" = "netcoreapp6.0" || "$NET_TEST_VERSION" = "netcoreapp7.0" ]]
+if [[ "$NET_TEST_VERSION" = "netcoreapp6.0" || "$NET_TEST_VERSION" = "netcoreapp7.0" || "$NET_TEST_VERSION" = "netcoreapp8.0" ]]
 then
-  TRX2JUNIT_VERSION="2.0.4"
+  TRX2JUNIT_VERSION="2.1.0"
 else
   dotnet sln remove Examples/ExampleBlazor/ExampleBlazor.csproj
 fi
