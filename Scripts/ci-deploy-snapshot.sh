ec1660c54ca75e0ade85f0c2c0e5820474d18548
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2026-03-10 14:49:24 +0700

    ci: wip

--- Scripts/ci-deploy-snapshot.sh
+++ Scripts/ci-deploy-snapshot.sh
@@ -11,11 +11,12 @@ BONITOO_NUGET_URL="${BONITOO_NUGET_URL:-$DEFAULT_BONITOO_NUGET_URL}"
 #
 # Deploy to Preview repository
 #
-dotnet pack Client.Core/Client.Core.csproj -c Debug -p:Version=5.1.0-dev.$CIRCLE_BUILD_NUM
-dotnet pack Client.Legacy/Client.Legacy.csproj -c Debug -p:Version=5.1.0-dev.$CIRCLE_BUILD_NUM
-dotnet pack Client --version-suffix=dev.$CIRCLE_BUILD_NUM
+dotnet pack Client.Core/Client.Core.csproj -c Debug -p:Version=$Version-dev.$CIRCLE_BUILD_NUM
+dotnet pack Client.Legacy/Client.Legacy.csproj -c Debug -p:Version=$Version-dev.$CIRCLE_BUILD_NUM
+dotnet pack Client.Linq/Client.Linq.csproj -c Debug -p:Version=$Version-dev.$CIRCLE_BUILD_NUM
+dotnet pack Client/Client.csproj -c Debug -p:Version=$Version-dev.$CIRCLE_BUILD_NUM
 
-dotnet pack Client.Linq/Client.Linq.csproj -c Debug -p:Version=5.1.0-dev.$CIRCLE_BUILD_NUM
+dotnet pack Client --version-suffix=dev.$CIRCLE_BUILD_NUM
 
 dotnet nuget push ./Client.Core/bin/Debug/InfluxDB.Client.Core.*-dev.$CIRCLE_BUILD_NUM.nupkg -s ${BONITOO_NUGET_URL} -k ${BONITOO_SNAPSHOT_APIKEY} -sk ${BONITOO_SNAPSHOT_APIKEY}
 dotnet nuget push ./Client.Legacy/bin/Debug/InfluxDB.Client.Flux.*-dev.$CIRCLE_BUILD_NUM.nupkg -s ${BONITOO_NUGET_URL} -k ${BONITOO_SNAPSHOT_APIKEY} -sk ${BONITOO_SNAPSHOT_APIKEY}
